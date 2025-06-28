3396f55054acbb8892656a9b8ef7bcffe08ac72d
ERFa1z <harunokei78@gmail.com>
2025-06-28 12:58:58 +0700

    perbaikan Fixes #22

--- app/Models/PenggunaModel.php
+++ app/Models/PenggunaModel.php
@@ -1,14 +1,21 @@
-<?php namespace App\Models;
+<?php
+
+namespace App\Models;
 
 use CodeIgniter\Model;
 
-class PengajuanModel extends Model
+class PenggunaModel extends Model
 {
-    protected $table      = 'pengajuan';
-    protected $primaryKey = 'idpengajuan';
-    protected $allowedFields = [
-        'idpengguna', 'namawisata', 'deskripsi', 'alamat', 'foto', 'idkategori'
-    ];
+    protected $table = 'pengguna';
+    protected $primaryKey = 'idpengguna';
 
+    protected $allowedFields = ['username', 'email', 'password', 'role'];
+    public function findByUsernameOrEmail($login)
+{
+    return $this->where('username', $login)
+                ->orWhere('email', $login)
+                ->first();
+}
 
 }
+
